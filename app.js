9fc20edd9f31185c8c2e1a5b173dc87e78057ff9
Neha Rai <neharadialcode@gmail.com>
2025-03-24 11:59:42 +0530

    mobile sidebar

--- app.js
+++ app.js
@@ -180,12 +180,39 @@ function displayProperties(properties) {
                 href="${property.link}"
                 target="_blank"
               >
+              <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="14"
+            viewBox="0 0 20 20"
+            fill="none"
+          >
+            <path
+              d="M13.3337 1.66675H6.66699C3.90949 1.66675 1.66699 3.90925 1.66699 6.66675V17.5001C1.66699 17.7211 1.75479 17.9331 1.91107 18.0893C2.06735 18.2456 2.27931 18.3334 2.50033 18.3334H13.3337C16.0912 18.3334 18.3337 16.0909 18.3337 13.3334V6.66675C18.3337 3.90925 16.0912 1.66675 13.3337 1.66675ZM16.667 13.3334C16.667 15.1717 15.172 16.6667 13.3337 16.6667H3.33366V6.66675C3.33366 4.82841 4.82866 3.33341 6.66699 3.33341H13.3337C15.172 3.33341 16.667 4.82841 16.667 6.66675V13.3334Z"
+              fill="white"
+            ></path>
+            <path
+              d="M5.83301 7.5H14.1663V9.16667H5.83301V7.5ZM5.83301 10.8333H11.6663V12.5H5.83301V10.8333Z"
+              fill="white"
+            ></path>
+          </svg>
                 View Details
               </a>
               <a
                 class="gsk_profile_btn"
                 href="${property.acf.book_an_appointment_link || "#"}"
-              >
+              >      <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="14"
+            viewBox="0 0 20 20"
+            fill="none"
+          >
+            <path
+              d="M2.50033 17.1583L5.49199 14.1667H15.0003C15.4424 14.1667 15.8663 13.9911 16.1788 13.6785C16.4914 13.3659 16.667 12.942 16.667 12.5V5C16.667 4.55797 16.4914 4.13405 16.1788 3.82149C15.8663 3.50893 15.4424 3.33333 15.0003 3.33333H4.16699C3.72496 3.33333 3.30104 3.50893 2.98848 3.82149C2.67592 4.13405 2.50033 4.55797 2.50033 5V17.1583ZM2.50033 18.3333H1.66699V5C1.66699 4.33696 1.93038 3.70107 2.39923 3.23223C2.86807 2.76339 3.50395 2.5 4.16699 2.5H15.0003C15.6634 2.5 16.2993 2.76339 16.7681 3.23223C17.2369 3.70107 17.5003 4.33696 17.5003 5V12.5C17.5003 13.163 17.2369 13.7989 16.7681 14.2678C16.2993 14.7366 15.6634 15 15.0003 15H5.83366L2.50033 18.3333Z"
+              fill="#002A52"
+            ></path>
+          </svg>
                 Apply Now
               </a>
             </div>
@@ -285,4 +312,24 @@ maxInput.addEventListener("change", () => {
   }
 
   range.noUiSlider.set([null, maxValue]);
+});
+
+// SIDEBAR =============================================
+
+const btn = document.getElementById("gsk_mobile_filter_btn");
+const overlay = document.getElementById("gsk_mobile_overlay");
+const cross = document.getElementById("gsk_mobile_cross");
+const sidebar = document.getElementById("gsk_mobile_sidebar");
+btn.addEventListener("click", function () {
+  sidebar.style.left = "0px";
+  overlay.style.display = "block";
+});
+
+overlay.addEventListener("click", function () {
+  sidebar.style.left = "-400px";
+  overlay.style.display = "none";
+});
+cross.addEventListener("click", function () {
+  sidebar.style.left = "-400px";
+  overlay.style.display = "none";
 });
